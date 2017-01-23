79b745e35e9e2245f4de47192ab2f986c68ea2a5
Mourad REDJAH <Mourad.REDJAH@Digital-Grenoble.local>
2017-01-23 09:58:42 +0100

    Exercices jQuery done for Validation + comments

--- script.js
+++ script.js
@@ -2,22 +2,22 @@ $( document ).ready(function() {
     console.log( "ready!" );
 
     /* section 1 */
-    $('#s1 h2').html("La bonne cible");
-    $('#s1_paragraphe').html("Je suis un paragraphe");
-    $('#s2_success').addClass("alert alert-success");
-    $('#s2_info').addClass("alert alert-info");
-    $('#s2_warning').addClass("alert alert-warning");
-    $('#s2_danger').addClass("alert alert-danger");
+    $('#s1 h2').html("La bonne cible");//Display txt in the id target
+    $('#s1_paragraphe').html("Je suis un paragraphe");//Display txt in the id target
+    $('#s2_success').addClass("alert alert-success");//Display bootstrap classes in the id target
+    $('#s2_info').addClass("alert alert-info");//Display bootstrap classes in the id target
+    $('#s2_warning').addClass("alert alert-warning");//Display bootstrap classes in the id target
+    $('#s2_danger').addClass("alert alert-danger");//Display bootstrap classes in the id target
     
     var s1_messageToAlert = $('#s1_messageToAlert').html();
     console.log(s1_messageToAlert);
-    // alert(s1_messageToAlert);
+    alert(s1_messageToAlert);
 
 
 
     /* section 2 */
-    $('#s2_source').keyup(function() {
-    	$('#s2_destination').html($(this).val());
+    $('#s2_source').keyup(function() { //Display typed text in s2_source
+    	$('#s2_destination').html($(this).val());//Display typed text in s2_source into s2_destination
     });
 
 
@@ -26,11 +26,13 @@ $( document ).ready(function() {
     $('#s2_number').change(function() {
      	$('#s2_pos').html($(this).val());
     	if ($(this).val() == 10) {
-    		$('#s2_pos').attr('class', 'label label-success')
+    		$('#s2_pos').attr('class', 'label label-success')//Give attr bootstrap classes
     	} else if ($(this).val() == -10) {
-    		$('#s2_pos').attr('class', 'label label-danger')
+    		$('#s2_pos').attr('class', 'label label-danger')//Give attr bootstrap classes
+        } else if ($(this).val() == -10) {
     	} else {
-    		$('#s2_pos').attr('class', 'label label-default')
+    		$('#s2_pos').attr('class', 'label label-default')//Give attr bootstrap classes
+        } else if ($(this).val() == -10) {
     	}
 	});
 
